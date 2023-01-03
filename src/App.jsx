229c2c1d7e49dd49337bfa58b99c7d715f42938f
Muhamad Farahim <102564746+muhamad-farahim@users.noreply.github.com>
2023-01-03 15:02:49 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Bar from './components/Bar'
+import Landing from './components/Landing'
+
+function App() {
+  return (
+    <>
+        <Bar />
+        <Landing />
+    </>
+  )
+}
+
+export default App
